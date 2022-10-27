4f5d1a7a73770a482a277c6b25bd2c4831cdf05d
barbosaisa <106408514+barbosaisa@users.noreply.github.com>
2022-10-26 21:12:53 -0300

    Add files via upload

---
+++ ATIVIDADE AULA 3 SIGNOS/script.js
@@ -0,0 +1,33 @@
+var signo = promp('Digite o seu signo ')
+switch (signo){
+   
+    case  'aries' :
+        console.log('de 21 março a 20 abril');
+        break;
+       
+
+     case 'Touro' :
+      console.log (' de 21 abril a 20 maio');
+      break;
+
+      
+     case 'Gemeos' :
+        console.log (' de 21 maio a 20 junho ');
+        break;
+ 
+    case 'cancer' :
+            console.log (' de 21 maio a 20 junho ');
+            break;
+     
+     case 'leao' :
+                console.log (' de 21 maio a 20 junho ');
+                break;
+         
+        case 'virgem' :
+                    console.log (' de 21 maio a 20 junho ');
+                    break;
+                
+                   default: 
+                console.log('signo não registrado')
+                        
+                                
